9aaa24770be1a56f06e3ac0ba9b28cba07c9a6c7
raystrike <raymond.sloan@ntlworld.com>
2017-10-30 21:53:31 +0000

    Basic PC Controls
    Pc Controls added to allow shooting ball towards mouse with simple shot projection

---
+++ Assets/Scripts/S_Ball.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_Ball : MonoBehaviour {
+
+    public float thrust;
+    public float fallOffMultiplier;
+    public Rigidbody2D rb;
+
+    public bool MidShot = false;
+
+	// Use this for initialization
+	void Start () 
+    {
+        rb = GetComponent<Rigidbody2D>();	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        //Adds Force
+        //rb.AddForce(transform.up * thrust);
+        //rb.velocity = thrust * transform.up;
+
+        var mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+        Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
+        transform.rotation = rot;
+
+        transform.rotation = Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z);
+
+        if (MidShot == false)
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
+        else if (MidShot == true)
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+        }
+
+        if (Input.GetMouseButtonDown(0) && MidShot == false)
+        {
+            rb.velocity = thrust * transform.up;
+            //MidShot = true;
+        }
+
+
+	}
+}
