74dece9bc916d0fbb604800b80388dbd22bf3c1b
splatkins <splatkins@users.noreply.github.com>
2017-11-06 14:54:33 +0000

    Borders finished and shot count started
    Borders implemented for screen height, needs to be updated for level height eventually. Shot count text added and ball physics updated to allow more shots once velocity = 0.

--- Assets/Scripts/S_Ball.cs
+++ Assets/Scripts/S_Ball.cs
@@ -7,7 +7,7 @@ public class S_Ball : MonoBehaviour {
     public float thrust;
     public float fallOffMultiplier;
     public Rigidbody2D rb;
-
+	public Vector2 velocity;
     public bool MidShot = false;
 
 	// Use this for initialization
@@ -22,6 +22,7 @@ public class S_Ball : MonoBehaviour {
         //Adds Force
         //rb.AddForce(transform.up * thrust);
         //rb.velocity = thrust * transform.up;
+		velocity = rb.velocity;
 
         var mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
         Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
@@ -41,9 +42,12 @@ public class S_Ball : MonoBehaviour {
         if (Input.GetMouseButtonDown(0) && MidShot == false)
         {
             rb.velocity = thrust * transform.up;
-            //MidShot = true;
+            MidShot = true;
         }
 
-
+		if (rb.velocity == new Vector2(0, 0))
+		{
+			MidShot = false;
+		}
 	}
 }
--- Assets/Scripts/S_Borders.cs
+++ Assets/Scripts/S_Borders.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class S_Borders : MonoBehaviour
 {
+	public float screenHeight;
+	public float myHeight;
 
 	// Use this for initialization
 	void Start ()
     {
-		
+		screenHeight = Screen.height;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+		myHeight = screenHeight / 10;
+		this.transform.localScale = new Vector3(1, myHeight, 0); // eventually change to scale to levelHeight as some levels will be taller than screen
 	}
 }
